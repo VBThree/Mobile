6c248ba021f5208d1fdcaf40b7dd0a2e240763c0
erturkgurses <erturkgurses@hotmail.com>
2020-09-14 23:10:04 +0300

    Added easy localization and generated routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,26 @@
+import 'package:VBThreeMobile/core/init/lang/language_manager.dart';
+import 'package:VBThreeMobile/core/init/navigation/router.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+import 'core/constants/app_constants.dart';
+
+void main() {
+  runApp(EasyLocalization(
+    child: MyApp(),
+    supportedLocales: LanguageManager.instance.supportedLocales,
+    path: ApplicationConstants.LANGUAGE_ASSET_PATH,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: Router.generateRoute,
+      initialRoute: homeRoute,
+    );
+  }
+}
