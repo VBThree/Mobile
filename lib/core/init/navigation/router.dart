b8989cb1a50375c9732aead19c899839c99ce4b3
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-09-15 23:38:00 +0300

    Added Login page router property

--- lib/core/init/navigation/router.dart
+++ lib/core/init/navigation/router.dart
@@ -1,5 +1,6 @@
 import 'package:VBThreeMobile/views/onboarding/onboardingPage.dart';
 import 'package:VBThreeMobile/views/wrong_route_page/wrong_route_page_view.dart';
+import 'package:VBThreeMobile/views/loginPage/loginPage.dart';
 import 'package:flutter/material.dart';
 
 class Router {
@@ -10,6 +11,10 @@ class Router {
           builder: (_) => OnboardingPage(),
         );
       case homeRoute:
+      case loginRoute:
+        return MaterialPageRoute(
+          builder: (_) => LoginPage(),
+        );
       default:
         return MaterialPageRoute(
           builder: (_) => WrongRouteView(),
@@ -20,3 +25,4 @@ class Router {
 
 const String homeRoute = '/';
 const String onboarding = '/onboarding';
+const String loginRoute = '/loginPage';
