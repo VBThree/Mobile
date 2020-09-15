c15ce4fd41907ba2969f52c85f6861ef6e986337
Yusuf Özgül <yozgul@icloud.com>
2020-09-15 21:23:23 +0300

    Added Onboarding Page

--- lib/core/init/navigation/router.dart
+++ lib/core/init/navigation/router.dart
@@ -1,9 +1,14 @@
+import 'package:VBThreeMobile/views/onboarding/onboardingPage.dart';
 import 'package:VBThreeMobile/views/wrong_route_page/wrong_route_page_view.dart';
 import 'package:flutter/material.dart';
 
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case onboarding:
+        return MaterialPageRoute(
+          builder: (_) => OnboardingPage(),
+        );
       case homeRoute:
       default:
         return MaterialPageRoute(
@@ -14,3 +19,4 @@ class Router {
 }
 
 const String homeRoute = '/';
+const String onboarding = '/onboarding';
