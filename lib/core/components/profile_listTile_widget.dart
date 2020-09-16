693b1ce80f723e2a4876d9eadd35bfd1d435da14
erturkgurses <erturkgurses@hotmail.com>
2020-09-17 01:07:44 +0300

    Changed ui for responsivity

--- lib/core/components/profile_listTile_widget.dart
+++ lib/core/components/profile_listTile_widget.dart
@@ -1,4 +1,3 @@
-import 'package:VBThreeMobile/core/base/view/base_view.dart';
 import 'package:VBThreeMobile/core/constants/color.dart';
 import 'package:VBThreeMobile/core/constants/radius.dart';
 import 'package:flutter/material.dart';
@@ -7,27 +6,36 @@ class ProfileListTile extends StatelessWidget {
   IconData icon;
   String title;
   int score;
+
   ProfileListTile(this.icon, this.title, this.score);
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Row(
       children: [
-        Icon(icon, size: 28, color: PROFILE_DARK_GREY_BLUE),
-        SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-        Text(title,
-            style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
-                color: PROFILE_DARK_GREY_BLUE)),
-        Spacer(),
+        Expanded(
+            child:
+                Icon(icon, size: 28, color: AllColors.PROFILE_DARK_GREY_BLUE),
+            flex: 2),
+        SizedBox(width: width * 0.05),
+        Expanded(
+            child: Text(title,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: height * 0.02,
+                    color: AllColors.PROFILE_DARK_GREY_BLUE)),
+            flex: 5),
+        Spacer(flex: 5),
         Container(
+          height: height * 0.06,
+          width: height * 0.06,
           child: Center(
               child: Text(
             score.toString(),
-            style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+            style:
+                TextStyle(fontSize: height * 0.02, fontWeight: FontWeight.w600),
           )),
-          width: 50,
-          height: 50,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(PROFILE_LIST_TILE_RADIUS),
               color: Colors.white,
@@ -42,7 +50,7 @@ class ProfileListTile extends StatelessWidget {
         ),
         Padding(
             padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).size.height * 0.08,
+          bottom: height * 0.08,
         ))
       ],
     );
