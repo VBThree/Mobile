d085d98f128dbb23970560bce8a24176d6c097e1
erturkgurses <erturkgurses@hotmail.com>
2020-09-17 01:12:45 +0300

    Created color pallete

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllColors {
+  static const Color GREY_FOR_BOX_SHADOW = Color(0x000000);
+  static const Color PROFILE_LIGHT_PEACH = Color(0xFFffc7ad);
+  static const Color PROFILE_APPBAR_ROSE_PINK = Color(0xFF534d4d);
+  static const Color PROFILE_APPBAR_GREYISH_BROWN = Color(0xFF00818a);
+  static const Color PROFILE_APPBAR_DUSK_BLUE = Color(0xFF216583);
+  static const Color PROFILE_DARK_GREY_BLUE = Color(0xFF353458);
+  static const Color PROFILE_BROWN_GRAY24 = Color(0xFFa8a8a8);
+  static const Color PROFILE_OCEAN = Color(0xFF00818a);
+  static const Color PROFILE_TWILIGHT = Color(0xFF525c8d);
+  static const Color PROFILE_ROSE_PINK = Color(0xFFf28891);
+  static const Color PROFILE_BLOWISH_GRAY = Color(0xFF625e5e);
+}
