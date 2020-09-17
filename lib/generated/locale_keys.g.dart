01b34cfe2b65902ec26d0fbd5bf5e39d907aa630
basakgunay <49794486+basakgunay@users.noreply.github.com>
2020-09-17 11:49:18 +0300

    locale_keys strings added

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -8,13 +8,15 @@ abstract class  LocaleKeys {
   static const NavBar_DarkMode = 'NavBar.DarkMode';
   static const NavBar_Logout = 'NavBar.Logout';
   static const NavBar = 'NavBar';
-
+  static const ForgotPassword_forgotton_password_title = 'ForgotPassword.forgotton_password_title';
+  static const ForgotPassword_send_forgotPage = 'ForgotPassword.send_forgotPage';
+  static const ForgotPassword_email_forgotPage = 'ForgotPassword.email_forgotPage';
+  static const ForgotPassword = 'ForgotPassword';
   static const signIn = 'signIn';
   static const email = 'email';
   static const password = 'password';
   static const forgotPassword = 'forgotPassword';
   static const dontHaveAnAccount = 'dontHaveAnAccount';
   static const signUp = 'signUp';
-
 
 }
