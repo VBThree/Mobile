1d1559efe02276cffe832a1a4841b3e2ec8332cf
mertcb <mertcan@ilao.co>
2020-09-17 21:00:08 +0300

    unable to test, you can test it i guess it's ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:VBThreeMobile/core/init/lang/language_manager.dart';
 import 'package:VBThreeMobile/core/init/navigation/router.dart';
-import 'package:VBThreeMobile/views/forgotPassword/forgotPassword_code.dart';
+//import 'package:VBThreeMobile/views/forgotPassword/forgotPassword_code.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
